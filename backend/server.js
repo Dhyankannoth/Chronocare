758001fd5752130c9565fa1348c258cbcca17c0d
Dhyankannoth <kannothdhyan.gmail.com>
2026-05-17 01:45:49 +0530

    Initial commit for Chronocare project

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const app = require("./src/app.js");
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Chronocare api is running on port ${PORT}`);
+});
+
